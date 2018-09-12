01d4e4fce262a604af425b9b60fcfc77eebb3d64
Patrycja <patrycjalucja@gmail.com>
2018-09-12 20:33:50 +0200

    added post functionality

--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Trial
+from django.utils import  timezone
 
 def post_list(request):
-    return render(request, 'main/post_list.html', {})
+    posts = Trial.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'main/post_list.html', {'posts': posts})
