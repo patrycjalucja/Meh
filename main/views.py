c0119752e7f955ea4302ab039cf7d5823a9b5349
Patrycja <patrycjalucja@gmail.com>
2018-10-14 15:57:19 +0200

    deleted django_ajax

--- main/views.py
+++ main/views.py
@@ -2,13 +2,6 @@ from django.shortcuts import render
 from .models import Trial
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import TemplateView
-from django_ajax.mixin import AJAXMixin
-
-
-class SimpleView(AJAXMixin, TemplateView):
-    template_name = 'base.html'
-    print("wywoluje sie")
 
 
 def post_list(request):
