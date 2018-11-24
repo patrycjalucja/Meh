2eef733a28c324bd0a00ab909668d314a2a21aa4
Patrycja <patrycjalucja@gmail.com>
2018-11-24 15:59:00 +0100

    added message generation

--- main/forms.py
+++ main/forms.py
@@ -4,9 +4,6 @@ from django import forms
 class GuestForm(forms.Form):
     name = forms.CharField(label='Imię: ', max_length='30')
     surname = forms.CharField(label='Nazwisko: ', max_length='30')
-    bus = forms.CheckboxInput()
-    overnight = forms.CheckboxInput()
-    commments = forms.CharField(label='Comments')
-
-    def get_name_and_surname(self):
-        return self.name, self.surname
+    bus = forms.BooleanField()
+    overnight = forms.BooleanField()
+    comments = forms.CharField(label='Comments')
--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, get_object_or_404
 from .forms import GuestForm
 from django.http import *
 
+
 def post_list(request):
     posts = Trial.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'main/post_list.html', {'posts': posts})
@@ -15,14 +16,41 @@ def post_detail(request, pk):
     return render(request, 'main/post_detail.html', {'post': post})
 
 
-def send_email(request):
+def submit_button(request):
     if request.method == 'POST':
         form = GuestForm(request.POST)
-        print('poscik')
     else:
         form = GuestForm(request.GET)
-        print('co innego')
     if form.is_valid():
         print('ok')
-    return HttpResponse('dzieki, ' + form.cleaned_data[
-        'name'] + '. Ta część jest jeszcze w budowie i potwierdzenie NIE zostało wysłane.')
+
+    message = generate_mail(form)
+    return HttpResponse('Tresc maila: ' + message)
+
+
+def generate_mail(form):
+    text = "Potwierdzam przybycie! " + form.cleaned_data['name'] + " " + form.cleaned_data['surname'] + ". "
+    try:
+        if form.cleaned_data['overnight']:
+            text += "Chcę skorzystać z noclegu. "
+        else:
+            text += "Nie potrzebuję noclegu. "
+    except KeyError:
+        text += "Nie potrzebuję noclegu. "
+
+    try:
+        if form.cleaned_data['bus']:
+            text += "Chcę skorzystać z busa. "
+        else:
+            text += "Nie potrzebuję transportu."
+    except KeyError:
+        text += "Nie potrzebuję transportu."
+
+    text += "\nMoje uwagi: \n"
+    if form.cleaned_data['comments']:
+        text += form.cleaned_data['comments']
+    else:
+        text += "Brak uwag."
+    text += "\nUWAGA. JEST TO WERSJA TESTOWA I TWOJE POTWIERDZENIE NIE JEST JESZCZE WYSYŁANE!"
+
+    return text
